65d4daa6654d58d796f1168ece95f9330a540e37
Christopher Villalobos <cvillalobos@ufl.edu>
2020-11-24 13:10:52 -0500

    Initial commit
    * Functional aggregation script
    * Will aggregate all CSV files one level below the chosen directory (as
      per Pythia specification)
    * Removes invalid harvests
    * Calculates PRODUCTION in tonnes
    * Creates output directory if necessary

---
+++ aggregate-pythia-outputs.R
@@ -0,0 +1,46 @@
+#library(here)
+library(argparser)
+library(data.table)
+
+setwd(".")
+
+p <- argparser::arg_parser("Aggregate Pythia outputs for World Modelers(fixed)")
+p <- argparser::add_argument(p, "input", "Pythia output directory to aggregate")
+p <- argparser::add_argument(p, "output", "final output of the aggregated files")
+argv <- argparser::parse_args(p)
+
+suppressWarnings(in_dir <- normalizePath(argv$input))
+suppressWarnings(out_file <- normalizePath(argv$output))
+
+#in_dir <- here("work", "fen_tot0")
+#out_file <- here("work", "wmout.csv")
+
+if (!dir.exists(in_dir)) {
+  stop(sprintf("%s does not exist.", in_dir))
+}
+
+out_dir <- dirname(out_file)
+
+if (!dir.exists(out_dir)) {
+  dir.create(out_dir, recursive = TRUE)
+}
+
+flist <- list()
+dts <- list()
+print("Loading files for aggregation.")
+fdirs <- list.dirs(path = in_dir)
+for(d in fdirs) {
+  f <- list.files(path = d, pattern = "*.csv", recursive = FALSE, full.names = TRUE)
+  flist <- c(flist, f)
+}
+for(f in flist) {
+  dts <- c(dts, list(data.table::fread(f)))
+}
+print("Starting aggregation.")
+df <- data.table::rbindlist(dts)
+valid_entries <- df[EDAT > 0 & MDAT > 0 & ADAT > 0 & HDAT > 0 & HWAH >= 0]
+calc_production <- valid_entries[, `:=`(PROD = HARVEST_AREA * HWAH, YEAR = trunc(HDAT/1000)), by = .(LATITUDE, LONGITUDE)]
+aggregated<- calc_production[,.(PRODUCTION=sum(PROD), HDATE=as.Date(paste0(mean(HDAT)), "%Y%j")), by = .(LATITUDE,LONGITUDE,YEAR)]
+final <- aggregated[,.(LATITUDE,LONGITUDE,PRODUCTION=round(PRODUCTION/1000),HDATE)]
+data.table::fwrite(final, file = out_file)
+print("Complete.")
