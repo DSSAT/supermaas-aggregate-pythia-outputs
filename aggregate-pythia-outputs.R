3d8981cf7a3beb36fbc052728e6422f530646bea
MengZhang <meng.zhang15@ufl.edu>
2021-02-17 12:07:43 -0500

    Implement capability of variable selection for aggregation script, still need discussion on calculation details for each variable except production

--- aggregate-pythia-outputs.R
+++ aggregate-pythia-outputs.R
@@ -7,6 +7,10 @@ setwd(".")
 p <- argparser::arg_parser("Aggregate Pythia outputs for World Modelers(fixed)")
 p <- argparser::add_argument(p, "input", "Pythia output directory to aggregate")
 p <- argparser::add_argument(p, "output", "final output of the aggregated files")
+p <- argparser::add_argument(p, "--variables", short="-v", nargs=Inf, help="Variable names for aggregation")
+# p <- argparser::add_argument(p, "--period_annual", short="-a", flag=TRUE, help="Do the aggregation by year")
+# p <- argparser::add_argument(p, "--period_month", short="-m", flag=TRUE, help="Do the aggregation by month")
+# p <- argparser::add_argument(p, "--period_season", short="-s", flag=TRUE, help="Do the aggregation by growing season")
 argv <- argparser::parse_args(p)
 
 suppressWarnings(in_dir <- normalizePath(argv$input))
@@ -14,6 +18,11 @@ suppressWarnings(out_file <- normalizePath(argv$output))
 
 #in_dir <- here("work", "fen_tot0")
 #out_file <- here("work", "wmout.csv")
+
+variables <- argv$variables
+suppressWarnings(if (is.na(variables)) {
+  variables <- "PRODUCTION";
+})
 
 if (!dir.exists(in_dir)) {
   stop(sprintf("%s does not exist.", in_dir))
@@ -32,13 +41,40 @@ flist <- list.files(path = in_dir, pattern = "*.csv", recursive = FALSE, full.na
 for(f in flist) {
   dts <- c(dts, list(data.table::fread(f)))
 }
-print("Starting aggregation.")
 df <- data.table::rbindlist(dts)
 valid_entries <- df[EDAT > 0 & MDAT > 0 & ADAT > 0 & HDAT > 0 & HWAH >= 0]
-calc_production <- valid_entries[, `:=`(PROD = HARVEST_AREA * HWAH, YEAR = trunc(HDAT/1000)), by = .(LATITUDE, LONGITUDE)]
-aggregated<- calc_production[,.(PRODUCTION=sum(PROD), HDATE=as.Date(paste0(mean(HDAT)), "%Y%j")), by = .(LATITUDE,LONGITUDE,YEAR)]
-final <- aggregated[,.(lat=LATITUDE,lng=LONGITUDE,timestamp=HDATE,production=round(PRODUCTION/1000))]
+print("Starting aggregation.")
+# if (argv$period_annual) {
+if (TRUE) {
+  print("Processing annual calculation.")
+  calc_production <- valid_entries[,`:=`(YEAR = trunc(HDAT/1000)), by = .(LATITUDE, LONGITUDE)]
+  aggregated<- calc_production[, .(HDATE=as.Date(paste0(mean(HDAT)), "%Y%j")), by = .(LATITUDE,LONGITUDE,YEAR)]
+  final <- aggregated[,.(lat=LATITUDE,lng=LONGITUDE,timestamp=HDATE)]
+  for (variable in variables) {
+    print(paste("Processing",  variable))
+    if (variable == "PRODUCTION") {
+      variable<-"PRODUCTION"
+      calc_production[,(variable) := HARVEST_AREA * HWAH, by = .(LATITUDE, LONGITUDE)]
+      calc_production[,(header):=sum(get(variable)), by = .(LATITUDE,LONGITUDE,YEAR)]
+      aggregated[, (variable):= calc_production[,get(variable)]]
+      final[, production := aggregated[,round(get(variable)/1000)]]
+    } else {
+      header <- paste0(variable, "_SUM")
+      calc_production[,(header):=sum(get(variable)), by = .(LATITUDE,LONGITUDE,YEAR)]
+      final[, (header):= calc_production[,get(header)]]
+    }
+    
+  }
+  
+  data.table::fwrite(final, file = out_file)
+}
+# if (argv$period_month) {
+#   #print("Processing monthly calculation.")
+#   print("Monthly aggregation has not been supported yet")
+# }
+# if (argv$period_season) {
+#   #print("Processing seasonal calculation.")
+#   print("Seasonal aggregation has not been supported yet")
+# }
 
-
-data.table::fwrite(final, file = out_file)
 print("Complete.")
