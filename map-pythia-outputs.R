48e21fc2c6666a45d353498353cc900708d5558b
MengZhang <meng.zhang15@ufl.edu>
2021-03-03 15:03:22 -0500

    Add dictionary function into map script and revise its help doc

--- map-pythia-outputs.R
+++ map-pythia-outputs.R
@@ -9,11 +9,22 @@ library(rnaturalearthdata) #for shapefiles, addition to gadm
 
 setwd(".")
 
+data_cde_file <- "DATA_CDE.csv"
+if (file.exists(data_cde_file)) {
+  var_dic <- data.table::fread(data_cde_file)
+} else {
+  # const_ha_vars <- c("DWAP", "CWAM", "HWAM", "HWAH", "BWAH", "PWAM", "")
+  # const_temp_vars <- c("TMAXA", "TMINA")
+  # const_date_vars <- c("SDAT", "PDAT", "EDAT", "ADAT", "MDAT", "HDAT")
+}
+
+predefined_vars <- c("PRODUCTION")
+
 p <- argparser::arg_parser("Generate Map with Aggregated Pythia outputs for World Modelers")
 p <- argparser::add_argument(p, "input_csv", "Aggregated Pythia result CSV file for generating map")
 p <- argparser::add_argument(p, "input_shape", "Shape file for generating map")
 p <- argparser::add_argument(p, "output", "Path to the generated map file, the file name is optional")
-p <- argparser::add_argument(p, "--variables", short = "-v", nargs = Inf, help = "Variable names for generating map")
+p <- argparser::add_argument(p, "--variables", short = "-v", nargs = Inf, help = paste("Variable names for generating map: [", paste(predefined_vars, var_dic[unit != "text", name], collapse = ","), "]"))
 argv <- argparser::parse_args(p)
 
 # for test only
@@ -26,7 +37,7 @@ suppressWarnings(out_file <- normalizePath(argv$output))
 
 variables <- argv$variables
 suppressWarnings(if (is.na(variables)) {
-  variables <- "PRODUCTION";
+  variables <- predefined_vars;
 })
 
 base_file_name = tools::file_path_sans_ext(basename(out_file))
@@ -51,33 +62,36 @@ Ylddata<-read.csv(in_csv) #average yield data with lat and long
 
 plot_yld_data <- st_as_sf(Ylddata, coords = c("lng", "lat"), crs =4326) #converting into spatial data
 st_crs(plot_yld_data)
-variable<-variables[1]
 for (variable in variables) {
   if (variable == "PRODUCTION") {
-    variable <- "production"
+    headers <- "production"
   } else {
-    variable <- paste0(variable, "_SUM");
+    headers <- paste0(variable, c("_TOT", "_AVG"));
   }
-  print(paste0("Processing map for ", variable, " ."))
-  ggplot()+
-    geom_sf(data=plot_yld_data, aes(color=get(variable)))+
-    geom_sf(data=shp_data, size=0.75, alpha=0.5, fill="lightgrey")+
-    labs(x="Longitude", y="Latitude", color=variable)+
-    scale_color_gradientn(colours = rainbow(7),
-                          n.breaks=6
-                          # ,labels=c(0,5, 10, 15, 20, 25)
-    )+
-    annotation_scale(location = "bl", width_hint = 0.4) +
-    annotation_north_arrow(location = "bl", which_north = "true", 
-                           pad_x = unit(0.05, "in"), pad_y = unit(0.25, "in"),
-                           style = north_arrow_fancy_orienteering)
-  
-  
-  ggsave(
-    filename = paste0(base_file_name, "_", variable, ".", file_ext),
-    plot = last_plot(),
-    path = out_dir
-  )
+  for (variable in headers) {
+    if (variable %in% plot_yld_data) {
+      print(paste0("Processing map for ", variable, " ."))
+      ggplot()+
+        geom_sf(data=plot_yld_data, aes(color=get(variable)))+
+        geom_sf(data=shp_data, size=0.75, alpha=0.5, fill="lightgrey")+
+        labs(x="Longitude", y="Latitude", color=variable)+
+        scale_color_gradientn(colours = rainbow(7),
+                              n.breaks=6
+                              # ,labels=c(0,5, 10, 15, 20, 25)
+        )+
+        annotation_scale(location = "bl", width_hint = 0.4) +
+        annotation_north_arrow(location = "bl", which_north = "true", 
+                               pad_x = unit(0.05, "in"), pad_y = unit(0.25, "in"),
+                               style = north_arrow_fancy_orienteering)
+      
+      
+      ggsave(
+        filename = paste0(base_file_name, "_", variable, ".", file_ext),
+        plot = last_plot(),
+        path = out_dir
+      )
+    }
+  }
 }
 print("Complete.")
 
