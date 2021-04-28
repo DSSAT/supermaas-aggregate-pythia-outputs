8370080649e4c93920957c826277b907d908b8a8
MengZhang <meng.zhang15@ufl.edu>
2021-04-28 13:05:25 -0400

    Add deployment for GADM shape files for the aggregation based on countries and regions

--- install-deps.R
+++ install-deps.R
@@ -1,1 +1,14 @@
 renv::restore(prompt=FALSE)
+
+# Download and deploy GADM shape files
+gadmDir <- "gadm_shapes"
+if (!dir.exists(gadmDir)) {
+  dir.create(path=gadmDir, recursive = T)
+  ### download the GADM shape file with given URL
+  gadmUrl = "https://biogeo.ucdavis.edu/data/gadm3.6/gadm36_levels_shp.zip"
+  gadmZip = file.path(gadmDir, "gadm36_levels_shp.zip")
+  download.file(url=gadmUrl, destfile=gadmZip)
+  unzip(zipfile=gadmZip, exdir = gadmDir,
+        files=grep(unzip(zipfile=gadmZip, list=TRUE)$Name, pattern="gadm36_1.*", value=TRUE, ignore.case = TRUE))
+  file.remove(gadmZip)
+}
