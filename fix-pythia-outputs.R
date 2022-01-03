d74d3681ce4382e1bec9e808126a8145705158df
MengZhang <meng.zhang15@ufl.edu>
2022-01-03 08:50:33 -0500

    Extend the capability to handle all the admin level calculation from GADM data. By default, it will calculate level 0 and 1.

--- fix-pythia-outputs.R
+++ fix-pythia-outputs.R
@@ -14,8 +14,9 @@ if (file.exists(data_cde_file)) {
   # const_date_vars <- c("SDAT", "PDAT", "EDAT", "ADAT", "MDAT", "HDAT")
 }
 
-resolveGeoPortion <- function (gadmShape, pixels, longDiff, latDiff, gridNum) {
+resolveGeoPortion <- function (gadmShape, pixels, longDiff, latDiff, gridNum, maxAdmLv) {
   # gridNum <- 1
+  admVars <- paste0("ADMLV", 0:maxAdmLv)
   if (gridNum > 1) {
     latIncr <- latDiff / gridNum
     longIncr <- longDiff / gridNum
@@ -35,12 +36,19 @@ resolveGeoPortion <- function (gadmShape, pixels, longDiff, latDiff, gridNum) {
   
   # calculate the admin level info
   indices <- over(pixelsSP, gadmShape)
-  pixelsXs[,`:=`(ADMLV0=indices$NAME_0,ADMLV1=indices$NAME_1)]
+  for (i in 0:maxAdmLv) {
+    pixelsXs[,(paste0("ADMLV", i)):=indices[paste0("NAME_", i)]]
+  }
+  # pixelsXs[,`:=`(ADMLV0=indices$NAME_0,ADMLV1=indices$NAME_1)]
   # Fix incorrect country name for PRC
-  pixelsXs[ADMLV0 %in% c("Hong Kong", "Taiwan", "Macao"), `:=`(ADMLV1 = ADMLV0, ADMLV0="China")]
+  if (maxAdmLv > 0) {
+    pixelsXs[ADMLV0 %in% c("Hong Kong", "Taiwan", "Macao"), `:=`(ADMLV1 = ADMLV0, ADMLV0="China")]
+  }
+  
   # reorganize data
-  pixelsXs <- pixelsXs[,.SD[!is.na(ADMLV1), .(ADMLV0, ADMLV1, total=.N)], by=.(LONGITUDE_ORG, LATITUDE_ORG)]
-  pixelsXs <- pixelsXs[,.(portion=.N/mean(total)),by=.(LONGITUDE_ORG, LATITUDE_ORG, ADMLV0, ADMLV1)]
+  pixelsXs <- pixelsXs[,.SD[!is.na(get(paste0("ADMLV", maxAdmLv))),.(cnt=.N), by=c(admVars, "LONGITUDE_ORG", "LATITUDE_ORG")]]
+  pixelsXs <- merge(pixelsXs, pixelsXs[,.(total=sum(cnt)),by=.(LONGITUDE_ORG, LATITUDE_ORG)], by=c("LONGITUDE_ORG", "LATITUDE_ORG"), sort = F)
+  pixelsXs[,portion := cnt / total][,cnt := NULL][,total := NULL]
   setnames(pixelsXs, "LONGITUDE_ORG", "LONGITUDE")
   setnames(pixelsXs, "LATITUDE_ORG", "LATITUDE")
   pixels <- merge(pixels, pixelsXs, by=c("LONGITUDE", "LATITUDE"), all=T)
@@ -52,19 +60,23 @@ p <- argparser::add_argument(p, "input", "Pythia output directory or file to agg
 p <- argparser::add_argument(p, "--keep_original", short="-o", flag = TRUE, help=paste0("Keep Overwrite the original file. If missing, will use 'modified_' as the prefix for the file name"))
 p <- argparser::add_argument(p, "--skip_admlvl", short="-a", flag = TRUE, help=paste0("Skip the calculation for admin level"))
 p <- argparser::add_argument(p, "--grid_num", short="-g", default = 5, help=paste0("Provide the number to divide current pixel into smaller grid to calculate the portion of admin level, and provide 1 to skip the calculation"))
+p <- argparser::add_argument(p, "--max_adm_level", short="-l", default = 1, help=paste0("Provide the maximum admin level you want to reach, the higher level will consume more time to finish"))
 argv <- argparser::parse_args(p)
 
 # for test only
 # argv <- argparser::parse_args(p, c("test\\data\\case1", "-o", "test\\data\\case1", "-a"))
 # argv <- argparser::parse_args(p, c("test\\data\\case10\\pp_GGCMI_Maize_ir.csv", "-o", "-g", "6"))
 # argv <- argparser::parse_args(p, c("test\\data\\case12\\Maize_Belg", "-o", "-g", "6"))
-# argv <- argparser::parse_args(p, c("test\\data\\case17", "-o", "-g", "6"))
+# argv <- argparser::parse_args(p, c("test\\data\\case17\\base", "-o", "-g", "6", "-l", "2"))
 
 suppressWarnings(in_dir <- normalizePath(argv$input))
 
 isKeepOriginal <- argv$keep_original
 isSkipAdmlvl <- argv$skip_admlvl
 gridNum <- argv$grid_num
+maxAdmLv <- argv$max_adm_level
+
+admVars <- paste0("ADMLV", 0:maxAdmLv)
 
 if (gridNum < 0) {
   gridNum <- 1
@@ -90,7 +102,6 @@ if (!dir.exists(in_dir)) {
   flist <- in_dir
 } else {
   flist <- list.files(path = in_dir, pattern = "*.csv", recursive = FALSE, full.names = TRUE)
-  class(flist)
 }
 for(f in flist) {
   cat(paste0("Processing ", f, "\n"))
@@ -106,6 +117,11 @@ for(f in flist) {
   if (!"HYEAR" %in% colNames) {
     cat("Caculating HYEAR ...")
     valid_entries[,`:=`(HYEAR = trunc(HDAT/1000))]
+    cat("done\n")
+  }
+  if (!"HMONTH" %in% colNames) {
+    cat("Caculating HMONTH ...")
+    valid_entries[,HMONTH:=format(as.Date(paste0(HDAT), "%Y%j"), "%m")]
     cat("done\n")
   }
   if (!"PYEAR" %in% colNames) {
@@ -133,12 +149,11 @@ for(f in flist) {
     valid_entries[,`:=`(HARVEST_AREA = 1)]
     cat("done\n")
   }
-  
-  if (!isSkipAdmlvl && (!"ADMLV0" %in% colNames || !"ADMLV1" %in% colNames)) {
+  if (!isSkipAdmlvl && (F %in% (admVars %in% colNames))) {
     cat("Caculating Admin Levels ...")
     
     # Use GADM whole world shape file to query the country and region names
-    gadmShape <- shapefile(file.path("gadm_shapes", "gadm36_1.shp"))
+    gadmShape <- shapefile(file.path("gadm_shapes", paste0("gadm36_", maxAdmLv, ".shp")))
     
     # proj4str <- CRS(proj4string(gadmShape))
     proj4str <- CRS("+init=epsg:4326")
@@ -148,19 +163,30 @@ for(f in flist) {
     
     latDiff <- pixels[,.SD[order(LATITUDE)][,.(diff = diff(LATITUDE))], by=LONGITUDE][,.N, by=diff][N==max(N), diff][1]
     longDiff <- pixels[,.SD[order(LONGITUDE)][,.(diff = diff(LONGITUDE))], by=LATITUDE][,.N, by=diff][N==max(N), diff][1]
-    pixels <- resolveGeoPortion(gadmShape, pixels, longDiff, latDiff, gridNum)
+    pixels <- resolveGeoPortion(gadmShape, pixels, longDiff, latDiff, gridNum, maxAdmLv)
     
     # Fix the edge pixels which might be located on the sea caused by resolution
     maxRetry <- 5 # maximum retry 5 times for searching the land
     cnt <- 1
-    while (pixels[is.na(ADMLV1), .N] > 0 && cnt <= maxRetry) {
-      pixelsFixed <- resolveGeoPortion(gadmShape, pixels[is.na(ADMLV1),.(LONGITUDE_ORG = LONGITUDE, LATITUDE_ORG = LATITUDE),by=.(LONGITUDE,LATITUDE)], longDiff, latDiff, gridNum + cnt)
-      pixels <- rbind(pixels[!is.na(ADMLV1)], pixelsFixed)
+    while (pixels[is.na(get(paste0("ADMLV", maxAdmLv))), .N] > 0 && cnt <= maxRetry) {
+      pixelsFixed <- resolveGeoPortion(gadmShape, pixels[is.na(get(paste0("ADMLV", maxAdmLv))),.(LONGITUDE_ORG = LONGITUDE, LATITUDE_ORG = LATITUDE),by=.(LONGITUDE,LATITUDE)], longDiff, latDiff, gridNum + cnt, maxAdmLv)
+      pixels <- rbind(pixels[!is.na(get(paste0("ADMLV", maxAdmLv)))], pixelsFixed)
       cnt <- cnt + 1
     }
     
     # Create factor column for aggregations
-    valid_entries <- merge(pixels[,.(LATITUDE,LONGITUDE,ADMLV0,ADMLV1,ADMLVP=portion)], valid_entries, by=c("LATITUDE","LONGITUDE"), all=T, allow.cartesian=TRUE, allow.by=.EACHI)
+    pixels[, LONGITUDE_ORG := NULL][, LATITUDE_ORG := NULL]
+    setnames(pixels, "portion", "ADMLVP")
+    if ("ADMLVP" %in% colNames) {
+      valid_entries[,ADMLVP := NULL]
+    }
+    for (admVar in admVars) {
+      if (admVar %in% colNames) {
+        valid_entries[, (admVar) := NULL]
+      }
+    }
+    
+    valid_entries <- merge(pixels, valid_entries, by=c("LATITUDE","LONGITUDE"), all=T, allow.cartesian=TRUE, allow.by=.EACHI)
     
     # update harvest area based on portion
     valid_entries[,HARVEST_AREA := HARVEST_AREA * ADMLVP]
@@ -175,3 +201,4 @@ for(f in flist) {
   
   data.table::fwrite(valid_entries, file = f)
 }
+print("Complete.")
