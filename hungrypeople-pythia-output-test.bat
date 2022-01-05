31e23b11eb215c175a9975a23d903486a328c123
MengZhang <meng.zhang15@ufl.edu>
2022-01-05 12:11:39 -0500

    remove unused code and fix bugs

--- hungrypeople-pythia-output-test.bat
+++ hungrypeople-pythia-output-test.bat
@@ -1,24 +1,2 @@
-
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_2.0.csv -m std -c 2.0
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_1.9.csv -m std -c 1.9
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_1.8.csv -m std -c 1.8
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_1.7.csv -m std -c 1.7
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_1.6.csv -m std -c 1.6
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_1.5.csv -m std -c 1.5
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_1.4.csv -m std -c 1.4
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_1.3.csv -m std -c 1.3
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_1.2.csv -m std -c 1.2
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_1.1.csv -m std -c 1.1
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_std_1.0.csv -m std -c 1.0
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_0.csv -m pctl -c 0
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_10.csv -m pctl -c 10
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_20.csv -m pctl -c 20
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_30.csv -m pctl -c 30
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_40.csv -m pctl -c 40
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_50.csv -m pctl -c 50
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_60.csv -m pctl -c 60
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_70.csv -m pctl -c 70
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_80.csv -m pctl -c 80
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_90.csv -m pctl -c 90
-Rscript hungrypeople-pythia-output.R test\data\case17\\base test\data\case17\\scenario test\data\case17\result\report_pctl_100.csv -m pctl -c 100
+Rscript hungrypeople-pythia-output.R test\data\case17\\scenario test\data\case17\result\report_p1_test.csv
 pause
--- hungrypeople-pythia-output.R
+++ hungrypeople-pythia-output.R
@@ -1,7 +1,6 @@
 #library(here)
 library(argparser)
 library(data.table)
-library(raster)
 
 setwd(".")
 
@@ -14,56 +13,36 @@ if (file.exists(data_cde_file)) {
   # const_date_vars <- c("SDAT", "PDAT", "EDAT", "ADAT", "MDAT", "HDAT")
 }
 
-# predefined_percentiles <- c(0, 5, 25,50, 75, 95, 100)
-# default_factors_threshold <- c("LATITUDE", "LONGITUDE", "RUN_NAME")
 default_factors_population <- c("LATITUDE", "LONGITUDE", "HYEAR")
 default_var <- "HWAH"
 
 p <- argparser::arg_parser("Estimate the population affected by crop failures based on Pythia outputs for World Modelers(fixed)")
-# p <- argparser::add_argument(p, "input_base", "Pythia output result directory or file for Baseline conditions")
 p <- argparser::add_argument(p, "input_fore", "Pythia output result directory or file for Forecast simulations")
 p <- argparser::add_argument(p, "output", "statistics report CSV file")
-# p <- argparser::add_argument(p, "--yield_threshold_method", short="-m", default="std", help=paste0("The method used for calculating yield threshold, options could be [std, pctl]"))
-# p <- argparser::add_argument(p, "--yield_threshold_constant", short="-c", default="1.5", help=paste0("The constant used for calculating yield threshold"))
 p <- argparser::add_argument(p, "--yield_threshold", short="-y", default="100", help=paste0("Threshold for low yield, kg/ha"))
 p <- argparser::add_argument(p, "--population_threshold", short="-p", default="95", help=paste0("Threshold for population, percent"))
 # p <- argparser::add_argument(p, "--boxplot", short="-b", help = "File Path to generaete box plot graph")
-p <- argparser::add_argument(p, "--variable", short="-v", nargs=1, help=paste0("Variable name for determine population affected by crop failure:, by default will be ", paste(default_vars, collapse = ",")))
-# p <- argparser::add_argument(p, "--factors_agg", short="-c", nargs=Inf, help=paste0("Factor names for aggregation: [", paste(unique(var_dic[factor!="", name]), collapse=","), "], by default will use factors for statistics plus HYEAR"))
-p <- argparser::add_argument(p, "--gadm_path", short="-g", default = "gadm_shapes", nargs=Inf, help="Path to the GADM shape file forlder")
+p <- argparser::add_argument(p, "--variable", short="-v", nargs=1, help=paste0("Variable name for determine population affected by crop failure:, by default will be ", paste(default_var, collapse = ",")))
+p <- argparser::add_argument(p, "--ignore_zero", short="-z", flag = TRUE, help=paste0("Ignore the rows with 0 hungry people in the output"))
 argv <- argparser::parse_args(p)
 
 # for test only
 # argv <- argparser::parse_args(p, c("test\\data\\case17\\base", "test\\data\\case17\\scenario", "test\\data\\case17\\result\\report_P1.csv", "-m", "std", "-c", "1.5"))
 # argv <- argparser::parse_args(p, c("test\\data\\case17\\base", "test\\data\\case17\\scenario", "test\\data\\case17\\result\\report_P_25.csv", "-m", "pctl", "-c", "25"))
 # argv <- argparser::parse_args(p, c("test\\data\\case17\\scenario", "test\\data\\case17\\result\\report_P1.csv" , "-y", "1000"))
-# argv <- argparser::parse_args(p, c("test\\data\\case17\\scenario", "test\\data\\case17\\result\\report_P1.csv"))
+# argv <- argparser::parse_args(p, c("test\\data\\case17\\scenario", "test\\data\\case17\\result\\report_P1.csv", "-z"))
 
-# suppressWarnings(in_dir_base <- normalizePath(argv$input_base))
 suppressWarnings(in_dir_fore <- normalizePath(argv$input_fore))
 suppressWarnings(out_file <- normalizePath(argv$output))
 
-# yield_threshold_method <- argv$yield_threshold_method
-# yield_threshold_constant <- as.numeric(argv$yield_threshold_constant)
 yield_threshold <- as.numeric(argv$yield_threshold)
 population_threshold <- as.numeric(argv$population_threshold) / 100
 variable <- argv$variable
+ignore_zero_flg <- argv$ignore_zero
 suppressWarnings(if (is.na(variable) || is.null(variable)) {
   variable <- default_var
 })
-# factors_threshold <- argv$factors_threshold
-# suppressWarnings(if (is.na(factors_threshold)) {
-#   factors_threshold <- default_factors_threshold
-# })
-# factors_population <- argv$factors_population
-# suppressWarnings(if (is.na(factors_population)) {
-#   factors_population <- default_factors_population
-# })
 factors_population <- default_factors_population
-
-# if (!dir.exists(in_dir_base) && !file.exists(in_dir_base)) {
-#   stop(sprintf("%s does not exist.", in_dir_base))
-# }
 
 if (!dir.exists(in_dir_fore) && !file.exists(in_dir_fore)) {
   stop(sprintf("%s does not exist.", in_dir_fore))
@@ -74,61 +53,6 @@ out_dir <- dirname(out_file)
 if (!dir.exists(out_dir)) {
   dir.create(out_dir, recursive = TRUE)
 }
-
-# # Process baseline data and calculate threshold
-# flist <- list()
-# dts <- list()
-# print("Loading files for calculation threshold from baseline conditions")
-# if (!dir.exists(in_dir_base)) {
-#   flist <- in_dir_base
-# } else {
-#   flist <- list.files(path = in_dir_base, pattern = "*.csv", recursive = FALSE, full.names = TRUE)
-# }
-# for(f in flist) {
-#   dts <- c(dts, list(data.table::fread(f)))
-# }
-# df <- data.table::rbindlist(dts)
-# 
-# valid_entries <- df
-# colNames <- colnames(valid_entries)
-# if ("EDAT" %in% colNames) {
-#   valid_entries <- valid_entries[!is.na(as.Date(paste0(EDAT), "%Y%j"))]
-# }
-# if ("MDAT" %in% colNames) {
-#   valid_entries <- valid_entries[!is.na(as.Date(paste0(MDAT), "%Y%j"))]
-# }
-# if ("ADAT" %in% colNames) {
-#   valid_entries <- valid_entries[!is.na(as.Date(paste0(ADAT), "%Y%j"))]
-# }
-# if ("HDAT" %in% colNames) {
-#   valid_entries <- valid_entries[!is.na(as.Date(paste0(HDAT), "%Y%j"))]
-# }
-# if ("HWAH" %in% colNames) {
-#   valid_entries <- valid_entries[HWAH > 0]
-# }
-# 
-# if (!"HYEAR" %in% colNames) {
-#   valid_entries[,`:=`(HYEAR = trunc(HDAT/1000))]
-# }
-# if (!"HMONTH" %in% colNames) {
-#   valid_entries[,HMONTH:=format(as.Date(paste0(HDAT), "%Y%j"), "%m")]
-# }
-# 
-# # if (!"PYEAR" %in% colNames) {
-# #   valid_entries[,`:=`(PYEAR = trunc(PDAT/1000))]
-# # }
-# 
-# print("Starting threshold calculation")
-# variable <- variables[1]
-# if (yield_threshold_method == "std") {
-#   baseline <- valid_entries[,.(THRESHOLD = mean(get(variable)) - yield_threshold_constant * sd(get(variable))),by = factors_threshold]
-#   # baseline[,threshold := (mean - (yield_threshold_constant)*std)]
-# } else if (yield_threshold_method == "pctl") {
-#   # yield_threshold_constant <- 25
-#   baseline <- valid_entries[,.(THRESHOLD = quantile(get(variable), probs = yield_threshold_constant/100)), by = factors_threshold]
-# } else {
-#   stop(sprintf("%s is not supported.", yield_threshold_method))
-# }
 
 # Process forecast data and calculate population affected by crop failure
 flist <- list()
@@ -192,30 +116,47 @@ if (!"HYEAR" %in% colNames) {
 # }
 
 print("Starting crop failure impact calculation")
-# fix : calcualte average of target varaible first, then compare to threshold
-# forecast <- merge(valid_entries, baseline, by=factors_threshold, all.x=T)
-columns <- c(factors_population,"ADMLV0", "ADMLV1", "HARVEST_AREA", "RUN_NAME", "HWAH", "POPULATION")
-forecast <- valid_entries[,..columns][POPULATION <= quantile(POPULATION, probs = population_threshold)]
+
+header <- var_dic[name==variable, average]
+aggFactors <- c(factors_population, "ADMLV0", "ADMLV1", "RUN_NAME")
+columns <- c(aggFactors, "HARVEST_AREA", "POPULATION")
+
+# calcualte average of target varaible first, then compare to threshold
+forecast <- valid_entries[,mean(get(variable)), by = aggFactors]
+setnames(forecast, "V1", header)
+forecast <- merge(forecast, valid_entries[,..columns], by=aggFactors, all.x =T, sort = F)
+
+# Exclude pixels with population > the threshold for further analysis. 
+forecast <- forecast[POPULATION <= quantile(POPULATION, probs = population_threshold)]
 forecast[, PIXEL_TOTAL_HARVEST_AREA := sum(HARVEST_AREA), by = factors_population]
 
-forecastLowHarvest <- forecast[get(variable) < yield_threshold]
+# Use the population map to compute the number of people affected by low harvests. For each pixel and each forecast year
+forecastLowHarvest <- forecast[get(header) < yield_threshold]
 forecastLowHarvest[, PIXEL_LOW_HARVEST_AREA := sum(HARVEST_AREA), by = factors_population]
 forecastLowHarvest[, HUNGRY_PEOPLE := POPULATION * PIXEL_LOW_HARVEST_AREA/ PIXEL_TOTAL_HARVEST_AREA]
 
-final_ADMLV0 <- forecastLowHarvest[,.(HUNGRY_PEOPLE = sum(HUNGRY_PEOPLE)), by = .(ADMLV0)]
-final_ADMLV0 <- merge(final_ADMLV0, forecast[,.(ADMLV0 = unique(ADMLV0))], by = "ADMLV0", all = T)
-final_ADMLV0[is.na(HUNGRY_PEOPLE), HUNGRY_PEOPLE := 0]
+# Aggregate the HungryPeoplei,n to Admin levels 0 for each year
+final_ADMLV0 <- forecastLowHarvest[,.(HUNGRY_PEOPLE = sum(HUNGRY_PEOPLE)), by = .(ADMLV0, HYEAR)][order(ADMLV0, HYEAR)]
+if (!ignore_zero_flg) {
+  final_ADMLV0 <- merge(final_ADMLV0, unique(forecast[,.(ADMLV0, HYEAR)]), by = c("ADMLV0", "HYEAR"), all = T, sort = F)
+  final_ADMLV0[is.na(HUNGRY_PEOPLE), HUNGRY_PEOPLE := 0]
+}
+setnames(final_ADMLV0, "HYEAR", var_dic[name=="HYEAR", factor])
 setnames(final_ADMLV0, "ADMLV0", var_dic[name=="ADMLV0", factor])
 setnames(final_ADMLV0, "HUNGRY_PEOPLE", "hungry_people")
-# final_ADMLV0 <- forecastLowHarvest[,.(ADMLV1 = "", HUNGRY_PEOPLE = sum(HUNGRY_PEOPLE)), by = .(ADMLV0)]
-final_ADMLV1 <- forecastLowHarvest[,.(HUNGRY_PEOPLE = sum(HUNGRY_PEOPLE)), by = .(ADMLV0, ADMLV1)]
-final_ADMLV1 <- merge(final_ADMLV1, unique(forecast[,.(ADMLV0, ADMLV1)]), by = c("ADMLV0", "ADMLV1"), all = T)
-final_ADMLV1[is.na(HUNGRY_PEOPLE), HUNGRY_PEOPLE := 0]
+
+# Aggregate the HungryPeoplei,n to Admin levels 0 for each year
+final_ADMLV1 <- forecastLowHarvest[,.(HUNGRY_PEOPLE = sum(HUNGRY_PEOPLE)), by = .(ADMLV0, ADMLV1, HYEAR)][order(ADMLV0, ADMLV1, HYEAR)]
+if (!ignore_zero_flg) {
+  final_ADMLV1 <- merge(final_ADMLV1, unique(forecast[,.(ADMLV0, ADMLV1, HYEAR)]), by = c("ADMLV0", "ADMLV1", "HYEAR"), all = T, sort = F)
+  final_ADMLV1[is.na(HUNGRY_PEOPLE), HUNGRY_PEOPLE := 0]
+}
+setnames(final_ADMLV1, "HYEAR", var_dic[name=="HYEAR", factor])
 setnames(final_ADMLV1, "ADMLV0", var_dic[name=="ADMLV0", factor])
 setnames(final_ADMLV1, "ADMLV1", var_dic[name=="ADMLV1", factor])
 setnames(final_ADMLV1, "HUNGRY_PEOPLE", "hungry_people")
 
-data.table::fwrite(rbind(final_ADMLV0, list("", "")), file = out_file)
+data.table::fwrite(rbind(final_ADMLV0, list("", "", "")), file = out_file)
 data.table::fwrite(final_ADMLV1, file = out_file, append = T, col.names = T)
 
 print("Complete.")
